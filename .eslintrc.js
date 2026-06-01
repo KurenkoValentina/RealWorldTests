d85dc07e0eef11a0793406c7d9b012942750d944
KurenkoValentina <104101845+KurenkoValentina@users.noreply.github.com>
2026-06-02 01:55:48 +0300

    Initial Playwright setup with example test

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:playwright/playwright-test'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // Можешь добавить свои правила, например:
+    'no-console': 'warn', // Предупреждать, если используешь console.log
+  },
+};
